ee153b9bd2ed4fce88d4b9defeff9cd6aee47e68
Andrea Boscolo Camiletto <abcamiletto@gmail.com>
2026-05-05 01:35:50 -0700

    Move shortlist help to footer

--- scripts/shortlist.sh
+++ scripts/shortlist.sh
@@ -105,7 +105,7 @@ selected="$(
   "$SHORTLIST_SCRIPT" list |
     fzf --prompt="Filter " --delimiter="\t" --with-nth="{2}  {3}  {4}" --nth=2,3,4 \
       --height=100% --layout=reverse --border=rounded \
-      --border-label=" tmux shortlist " --header="enter: jump | j/k: reorder | ctrl-x: remove | esc: close" \
+      --border-label=" tmux shortlist " --footer="enter: jump | j/k: reorder | ctrl-x: remove | esc: close" \
       --info=inline-right --pointer=">" --marker="+" \
       --preview="tmux capture-pane -ep -t {1} -S -60" \
       --preview-window=down,60%,border-top \
